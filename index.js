c70b2fdf191b07beff05cb2b59d5d392685e5e69
Scott Rodriguez <scottmrodriguez51@gmail.com>
2020-06-24 14:44:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  
+  return [key]
+} 
+function updateObjectWithKeyAndValue(object, key, value) {
+  
+}
+function updateObjectWithKeyAndValue(object, key, value) {
+  
+  return (key)
+}
+function destructivelyUpdateObjectWithKeyAndValue(){
+  
+}
+function deleteFromObjectByKey(object, key) {
+  
+}
+function delet
